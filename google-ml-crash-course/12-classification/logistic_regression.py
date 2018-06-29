bdddf14721e7e9d306a3b2fee6fb0bfcd73885b9
timur <timur.reziapov@gmail.com>
2018-06-29 13:56:29 -0700

    feat(logistic-regression): complete chapter

--- google-ml-crash-course/12-classification/logistic_regression.py
+++ google-ml-crash-course/12-classification/logistic_regression.py
@@ -18,6 +18,7 @@ from IPython import display
 from matplotlib import cm
 from matplotlib import gridspec
 from matplotlib import pyplot as plt
+from math import log
 import numpy as np
 import pandas as pd
 from sklearn import metrics
@@ -256,4 +257,176 @@ linear_regressor = train_linear_regressor_model(
         validation_examples=validation_examples,
         validation_targets=validation_targets
 )
+
+# Task 1: calculate log loss (if applicable).
+# Answer: we can
+predict_validation_input_fn = lambda: my_input_fn(
+        validation_examples,
+        validation_targets["median_house_value_is_high"],
+        num_epochs=1,
+        shuffle=False
+)
+validation_predictions = linear_regressor.predict(input_fn=predict_validation_input_fn)
+validation_predictions = np.array([item["predictions"][0] for item in validation_predictions])
+
+log_loss = 0.0
+for ypred, ytarget in zip(validation_predictions, validation_targets["median_house_value_is_high"]):
+  if ypred <= 0.000000:
+    ypred = 0.01
+  if ypred >= 1.00000:
+    ypred = 0.9999
+  try:
+    log_loss += -1.0 * ytarget * log(ypred) - (1.0 - ytarget) * log(1.0 - ypred)
+  except:
+    print("Error with Prediction: " + str(ypred) + ", target: " + str(ytarget))   
+    cotinue
+
+print("Log loss: " + str(log_loss))
+_ = plt.hist(validation_predictions)
+
+
+# Task 2: train a logistic regression model and calculate log loss on the validation set.
+def train_linear_classifier_model(
+    learning_rate,
+    steps,
+    batch_size,
+    training_examples,
+    training_targets,
+    validation_examples,
+    validation_targets):
+    """Trains a linear classification model.
+  
+    In addition to training, this function also prints training progress information,
+    as well as a plot of the training and validation loss over time.
+  
+    Args:
+        learning_rate: A `float`, the learning rate.
+        steps: A non-zero `int`, the total number of training steps. A training step
+            consists of a forward and backward pass using a single batch.
+        batch_size: A non-zero `int`, the batch size.
+        training_examples: A `DataFrame` containing one or more columns from
+            `california_housing_dataframe` to use as input features for training.
+        training_targets: A `DataFrame` containing exactly one column from
+            `california_housing_dataframe` to use as target for training.
+        validation_examples: A `DataFrame` containing one or more columns from
+            `california_housing_dataframe` to use as input features for validation.
+        validation_targets: A `DataFrame` containing exactly one column from
+            `california_housing_dataframe` to use as target for validation.
+      
+    Returns:
+        A `LinearClassifier` object trained on the training data.
+    """
+    periods = 10
+    steps_per_period = steps / periods
+
+    # Create a linear classifier object.
+    my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+    my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+    linear_classifier = tf.estimator.LinearClassifier(
+            feature_columns=construct_feature_columns(training_examples),
+            optimizer=my_optimizer
+    )
+
+    # Create input functions.
+    training_input_fn = lambda: my_input_fn(
+            training_examples,
+            training_targets["median_house_value_is_high"],
+            batch_size=batch_size
+    )
+    predict_training_input_fn = lambda: my_input_fn(
+            training_examples,
+            training_targets["median_house_value_is_high"],
+            num_epochs=1,
+            shuffle=False
+    )
+    predict_validation_input_fn = lambda: my_input_fn(
+            validation_examples,
+            validation_targets["median_house_value_is_high"],
+            num_epochs=1,
+            shuffle=False
+    )
+
+    # Train the model, but do so inside a loop so that we can periodically assess
+    # loss metrics.
+    print("Training model...")
+    print("LogLoss (on training data):")
+    training_log_losses = []
+    validation_log_losses = []
+    for period in range(0, periods):
+        # Train the model, starting from the prior state.
+        linear_classifier.train(
+            input_fn=training_input_fn,
+            steps=steps_per_period
+        )
+        # Take a break and compute predictions.
+        training_probabilities = linear_classifier.predict(input_fn=predict_training_input_fn)
+        training_probabilities = np.array([item['probabilities'] for item in training_probabilities])
+
+        validation_probabilities = linear_classifier.predict(input_fn=predict_validation_input_fn)
+        validation_probabilities = np.array([item['probabilities'] for item in validation_probabilities])
+
+        training_log_loss = metrics.log_loss(training_targets, training_probabilities)
+        validation_log_loss = metrics.log_loss(validation_targets, validation_probabilities)
+        
+        # Occasionally print the current loss.
+        print("  period %02d : %0.2f" % (period, training_log_loss))
+        # Add the loss metrics from this period to our list.
+        training_log_losses.append(training_log_loss)
+        validation_log_losses.append(validation_log_loss)
+
+    print("Model training finished.")
+
+    # Output a graph of loss metrics over periods.
+    plt.ylabel("LogLoss")
+    plt.xlabel("Periods")
+    plt.title("LogLoss vs. Periods")
+    plt.tight_layout()
+    plt.plot(training_log_losses, label="training")
+    plt.plot(validation_log_losses, label="validation")
+    plt.legend()
+
+    return linear_classifier
+
+linear_classifier = train_linear_classifier(
+    learning_rate=0.000005,
+    steps=500,
+    batch_size=20,
+    training_examples=training_examples,
+    training_targets=training_targets,
+    validation_examples=validation_examples,
+    validation_tragets=validation_targets
+)
+
+# Task 3: calculate accuracy and plot a ROC curve for the validation set
+evaluation_metrics = linear_classifier.evaluate(input_fn=predict_validation_input_fn)
+
+print('AUC on the validation set: %0.2f' % evaluation_metrics['auc'])
+print('Accuracy on the validation set: %0.2f' % evaludation_metrics['accuracy'])
+
+validation_probabilities = linear_classifier.predict(input_fn=predict_validation_input_fn)
+# Get just the probabilities for the positive class.
+validation_probabilities = np.array([item['probabilities'][1] for item in validation_probabilities])
+
+false_positive_rate, true_positive_rate, thresholds = metrics.roc_curve(
+    validation_targets,
+    validation_probabilities
+)
+plt.plot(false_positive_rate, true_positive_rate, label="our model")
+plt.plot([0, 1], [0, 1], label="random classifier")
+_ = plt.legend(loc=2)
+
+# Tuning for better results/metrics
+linear_classifier = train_linear_classifier_model(
+    learning_rate=0.000005,
+    steps=1000,
+    batch_size=30,
+    training_examples=training_examples,
+    training_targets=training_targets,
+    validation_examples=validation_examples,
+    validation_targets=validation_targets)
+
+evaluation_metrics = linear_classifier.evaluate(input_fn=predict_validation_input_fn)
+
+print("AUC on the validation set: %0.2f" % evaluation_metrics['auc'])
+print("Accuracy on the validation set: %0.2f" % evaluation_metrics['accuracy'])
 
